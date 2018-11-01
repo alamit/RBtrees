b6005aa5a8b714ad29bc9bd25b7be3e4230db4f8
alamit <adrien.lamit@effixis.ch>
2018-10-31 20:43:30 -0700

    refactor for better usability

--- src/tree_contains.c
+++ src/tree_contains.c
@@ -6,7 +6,7 @@
 /*   By: alamit <alamit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/30 17:11:30 by alamit            #+#    #+#             */
-/*   Updated: 2018/10/30 21:22:36 by alamit           ###   ########.fr       */
+/*   Updated: 2018/10/31 20:41:44 by alamit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 #include <tree_pvt.h>
 #include <stdlib.h>
 
-static	int	contains(t_node *node, int (*f)())
+static	int	contains(t_node *node, void *data, int (*cmp)())
 {
 	if (node == NULL)
 		return (0);
-	return (f(g_data(node)) ||
-			contains(left(node), f) ||
-			contains(right(node), f));	
+	return (cmp(g_data(node), data) == 0 ||
+			contains(left(node), data, cmp) ||
+			contains(right(node), data, cmp));
 }
 
-int			tree_contains(t_tree *tree, int (*f)())
+int			tree_contains(t_tree *tree, void *data)
 {
-	return (contains(tree->root, f));
+	return (contains(tree->root, data, tree->cmp));
 }
